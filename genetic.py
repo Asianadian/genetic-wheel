6fb05545c2f3ae565ee8ab36de3c0d8331f250c8
Isabella Nguyen <bella.pvi@gmail.com>
2024-11-27 16:13:46 -0500

    Add genetic algorithms

---
+++ genetic.py
@@ -0,0 +1,28 @@
+import random
+import numpy as np
+
+def genetic_split_row(x, y):
+    n, d = x.shape
+    split = random.randrange(1, n)
+
+    print(split)
+
+    g1 = x[:split]
+    g2 = y[split:]
+
+    return np.concatenate([g1, g2])
+
+def genetic_split_col(x, y):
+    n, d = x.shape
+
+    for row in range(n):
+        split = random.randrange(1, d)
+        print(split)
+
+        x[row][split:] = y[row][split:]
+
+    return x
+
+# print(genetic_split_col(np.array([[1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5]]), np.array([[6,7,8,9,10], [6,7,8,9,10], [6,7,8,9,10], [6,7,8,9,10], [6,7,8,9,10], [6,7,8,9,10], [6,7,8,9,10], [6,7,8,9,10]])))
+
+
