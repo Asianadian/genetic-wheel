da87c8659aaee2b7c80fd5ccd6214b54fa75ea9f
Friendly-Neighborhood-Programmer <isaacrobert1316@gmail.com>
2024-11-27 16:22:12 -0500

    add mutation function

---
+++ mutation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Mutate all the parameters of the wheel by an amount
+# the shape of the wheel changes with a small probability
+def mutate_full_wheel(wheel_matrix):
+    vertex_mutation_rate = 0.005
+
+    for i in range(wheel_matrix.shape[0]):
+        for j in range(wheel_matrix.shape[1]):
+            if np.random.rand() < vertex_mutation_rate:
+                if (wheel_matrix[i][j] == 0):
+                    wheel_matrix[i][j] = 1
+                else:
+                    wheel_matrix[i][j] = 0
+
+    for i in range(3):
+        wheel_matrix[101][i] = wheel_matrix[101][i] + np.random.rand() * 0.1 - 0.05
+                    
